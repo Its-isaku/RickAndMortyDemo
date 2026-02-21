74bccbb21593465bc1b407e796d3af8034f72e6a
Its-Isaku <Isai.almeraz@uabc.edu.mx>
2026-02-21 12:20:15 -0800

    Initial Commit

---
+++ RickAndMortyDemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickAndMortyDemo
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 21/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickAndMortyDemo/RickAndMortyDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyDemoApp.swift
+//  RickAndMortyDemo
+//
+//  Created by Isai Magdaleno Almeraz Landeros on 21/02/26.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
